f693f23371b87af12fc2a4133d4f73d7360a6fc5
Alyssa Rushing <alyhope@live.unc.edu>
2022-01-28 16:06:34 -0500

    Finished Ex.02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -10,26 +10,26 @@ guess: str = input("What is your " + (str(len(secret))) + "-letter guess? ")
 while len(secret) != len(guess):     # checking the lengths 
     guess = input("That was not " + str(len(secret)) + " letters! Try again: ")
 
-i: int = 0       # checking where to put emojis
+i: int = 0       # variable
 emoji: str = ""
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-exist: bool = False    # checking for yellow eomji placement
+exist: bool = False    
 alt: int = 0
 
-while i < len(secret):
+while i < len(secret):               # adding green boxes
     if (secret[i]) == (guess[i]):
         emoji = emoji + GREEN_BOX
         exist = True
     else:
-        while not exist and alt < len(secret):
-            if secret[alt] == guess[i]:
+        while not exist and alt < len(secret):     
+            if secret[alt] == guess[i]:            # adding yellow boxes
                 emoji = emoji + YELLOW_BOX
                 exist = True
             alt = alt + 1
     if not exist:
-        emoji = emoji + WHITE_BOX
+        emoji = emoji + WHITE_BOX         # adding white boxes
     alt = 0
     exist = False        
     i = i + 1
